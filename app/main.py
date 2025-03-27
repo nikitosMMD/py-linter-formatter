505d7af95de4f2cb66cbba69965c36576c222354
nikitosMMD <pronikito38@gmail.com>
2025-03-28 01:14:03 +0200

    Revert "Solution"

--- app/main.py
+++ app/main.py
@@ -1,20 +1,13 @@
 def format_linter_error(error: dict) -> dict:
-    return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
-        "source": "flake8"
-    }
+    # write your code here
+    pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {
-        "errors": [format_linter_error(error) for error in errors],
-        "path": file_path,
-        "status": "failed" if errors else "passed"
-    }
+    # write your code here
+    pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
+    # write your code here
+    pass
